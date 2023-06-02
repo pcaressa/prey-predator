4af692fcd6f10dcfad12d52af1e203e4e20de62c
pcaressa <paolo@caressa.it>
2023-06-02 18:11:49 +0200

    Introduced function "simulation"

--- prey_predator_simulation.py
+++ prey_predator_simulation.py
@@ -34,10 +34,13 @@ HERBIVORE = 2.0
 CARNIVORE = 3.0
 
 class Territory:
+
     def __init__(self, N, M):
         self.N = N
         self.M = M
+        self.CELL_WIDTH = 1024//max(N, M)
         self.grid = [[Nil() for i in range(M)] for j in range(N)]
+
     def __str__(self):
         t = "-"*self.M + "\n"
         for x in range(self.N):
@@ -54,14 +57,17 @@ class Territory:
             t += "\n"
         t += "-"*self.M + "\n"
         return t
+
     def get(self, x, y):
         """Retrieve the object located at the x,y coordinates
         in the territory and returns it."""
         return self.grid[x][y]
+
     def put(self, x, y, obj):
         """Insert the object obj into the territory at the
         given coordinates: the previous object is discarded."""
         self.grid[x][y] = obj
+
     def find_near(self, x, y, kind):
         """Returns the list coordinates of cells around (x,y)
         where there is an object of the given kind,
@@ -74,54 +80,45 @@ class Territory:
                 if x+i >= 0 and x+i < self.N and y+j >= 0 and y+j < self.M and self.grid[x+i][y+j].kind == kind:
                     neighbors.append((x+i,y+j))
         return neighbors
+
     def find_empty(self, x1=0, y1=0, x2=-1, y2=-1):
         """Finds an empty cell in the territory in the rectangle
         with left upper coordinate (x0,y0) and right lower
         coordinate (x1,y1)"""
         if x2 == -1: x2 = self.N
         if y2 == -1: y2 = self.M
-        for i in range(N*M):
+        for i in range(self.N*self.M):
             x = randrange(x1, x2)
             y = randrange(y1, y2)
             if self.get(x, y).kind == NIL:
                 return x, y
-            
-        """
-        x = randrange(x1, x2)
-        y = randrange(y1, y2)
-        for i in range(self.N*self.M):
-            if self.get(x, y).kind == NIL:
-                return x, y
-            x += 1
-            if x == self.N:
-                x = 0
-                y += 1
-                if y == self.M:
-                    y = 0
-        """
         raise "NO MORE SPACE FOR ANIMALS OR PLANTS!!!"
+
     def display(self, canvas):
         for y in range(self.N):
             for x in range(self.M):
-                xx = CELL_WIDTH*x
-                yy = CELL_WIDTH*y
+                # Coordinates of rectangle to print
+                x0, y0 = self.CELL_WIDTH*x, self.CELL_WIDTH*y
+                x1, y1 = x0 + self.CELL_WIDTH, y0 + self.CELL_WIDTH
                 match self.grid[y][x].kind:
                     case 1.0:
-                        canvas.create_rectangle((xx, yy), (xx+CELL_WIDTH, yy+CELL_WIDTH), fill="green", outline="green")
+                        canvas.create_rectangle((x0, y0), (x1, y1),
+                                                fill="green", outline="green")
                     case 2.0:
-                        canvas.create_rectangle((xx, yy), (xx+CELL_WIDTH, yy+CELL_WIDTH), fill="grey", outline="grey")
+                        canvas.create_rectangle((x0, y0), (x1, y1),
+                                                fill="grey", outline="grey")
                     case 3.0:
-                        canvas.create_rectangle((xx, yy), (xx+CELL_WIDTH, yy+CELL_WIDTH), fill="red", outline="red")
+                        canvas.create_rectangle((x0, y0), (x1, y1),
+                                                fill="red", outline="red")
 
 class Nil:
-    def __init__(self):
-        self.kind = NIL
+    kind = NIL
 
 class Plant:
-    def __init__(self):
-        self.kind = PLANT
+    kind = PLANT
 
 class Animal:
+
     @classmethod
     def reproduce(c, territory, x, y):
         return c(territory, x, y)
@@ -131,9 +128,11 @@ class Animal:
         self.x = x
         self.y = y
         self.territory.put(x, y, self)
+
     def age(self):
         self.energy -= 1
         self.timespan -= 1
+
     def can_mate(self, month, spawn):
         if self.energy >= self.threshold and \
             (month%12) >= self.mate_start and \
@@ -143,16 +142,18 @@ class Animal:
             n = self.territory.find_near(self.x, self.y, NIL)
             if len(n) > 0:
                 x1, y1 = choice(n)
-                cub = self.reproduce(territory, x1,y1)
+                cub = self.reproduce(self.territory, x1,y1)
                 spawn.append(cub)
                 return True
         return False
+
     def is_dead(self):
         if self.energy < 0 or self.timespan < 0:
             self.energy = -1
             self.territory.put(self.x, self.y, Nil())
             return True
         return False
+
     def move_random(self):
         """An animal may move on an empty cell or on a cell where
         there is a plant."""
@@ -165,6 +166,7 @@ class Animal:
             if len(n) > 0:
                 x, y = choice(n)
                 self.move_to(x, y)
+
     def move_to(self, x, y):
         self.territory.put(self.x, self.y, Nil())
         self.x = x
@@ -172,6 +174,7 @@ class Animal:
         self.territory.put(x, y, self)
 
 class Herbivore(Animal):
+
     def __init__(self, territory, x, y):
         super().__init__(territory, x,y)
         self.energy = 8
@@ -180,8 +183,10 @@ class Herbivore(Animal):
         self.mate_start = 1
         self.mate_end = 4
         self.threshold = 3    # minimum amount of energy to spawn
+
     def __repr__(self):
         return f"Herbivore: x={self.x}, y={self.y}; energy={self.energy}; timespan={self.timespan}; start mate={self.mate_start}, end mate={self.mate_end}; threshold={self.threshold}"
+
     def can_eat(self):
         n = self.territory.find_near(self.x, self.y, PLANT)
         if len(n) > 0:
@@ -193,6 +198,7 @@ class Herbivore(Animal):
         return False
     
 class Carnivore(Animal):
+
     def __init__(self, territory, x, y):
         super().__init__(territory, x,y)
         self.energy = 12
@@ -201,8 +207,10 @@ class Carnivore(Animal):
         self.mate_start = 6
         self.mate_end = 11
         self.threshold = 6    # minimum amount of energy to spawn
+
     def __repr__(self):
         return f"Carnivore: x={self.x}, y={self.y}; energy={self.energy}; timespan={self.timespan}; start mate={self.mate_start}, end mate={self.mate_end}; threshold={self.threshold}"
+
     def can_eat(self):
         n = self.territory.find_near(self.x, self.y, HERBIVORE)
         # At least two herbivores should be at hand
@@ -214,6 +222,7 @@ class Carnivore(Animal):
             self.energy += 1
             return True
         return False
+
     def move_random(self):
         """A carnivore moves twice an herbivore."""
         super().move_random()
@@ -221,125 +230,126 @@ class Carnivore(Animal):
         super().move_random()
         super().move_random()
 
+def simulation(N: int,
+               M: int,
+               MAX_MONTH: int,
+               PLANT_RATIO: float,
+               HERBIVORE_RATIO: float,
+               CARNIVORE_RATIO: float):
+    CELL_WIDTH = 1024//max(N, M)
 
-# Main script starts here
+    n_plants = int(N*M * PLANT_RATIO)
+    n_herbivores = int(N*M * HERBIVORE_RATIO)
+    n_carnivores = int(N*M * CARNIVORE_RATIO)
 
-N = 100
-M = 100
+    # Set up the territory
+    territory = Territory(N, M)
 
-CELL_WIDTH = 5
+    # Set up living beings: plants are elements of the territory
+    for i in range(n_plants):
+        x, y = territory.find_empty()
+        territory.put(x, y, Plant())
 
-MAX_MONTH = 512
+    # Animals are collected lists and referenced in the territory
+    herbivores = []
+    for i in range(n_herbivores):
+        # Herbivores are located at North
+        x, y = territory.find_empty(0, 0, N//2, M)
+        animal = Herbivore(territory, x, y)
+        herbivores.append(animal)
 
-PLANT_RATIO = 0.5
-HERBIVORE_RATIO = 0.03
-CARNIVORE_RATIO = 0.01
+    carnivores = []
+    for i in range(n_carnivores):
+        # Carnivores are located at South
+        x, y = territory.find_empty(N//2+1, 0, N, M)
+        animal = Carnivore(territory, x, y)
+        carnivores.append(animal)
 
-n_plants = int(N*M * PLANT_RATIO)
-n_herbivores = int(N*M * HERBIVORE_RATIO)
-n_carnivores = int(N*M * CARNIVORE_RATIO)
+    print("Simulation with:")
+    print(f"\t{N*M} territory area")
+    print(f"\t{n_plants} plants")
+    print(f"\t{n_herbivores} herbivores")
+    print(f"\t{n_carnivores} carnivores")
+    print(f"Timespan: {MAX_MONTH/12} years")
+    print()
 
-# Set up the territory
-territory = Territory(N, M)
+    csvstring = "Time; Plants; Herbivores; Carnivores\n"
 
-# Set up living beings: plants are elements of the territory
-for i in range(n_plants):
-    x, y = territory.find_empty()
-    territory.put(x, y, Plant())
+    root = tk.Tk()
+    root.geometry(f"{CELL_WIDTH*M}x{CELL_WIDTH*N}")
+    root.title("Predator-prey simulation")
+    canvas = tk.Canvas(root, width = CELL_WIDTH*M, height = CELL_WIDTH*N, bg = "white")
+    canvas.pack(anchor = tk.CENTER, expand = True)
 
-# Animals are collected lists and referenced in the territory
-herbivores = []
-for i in range(n_herbivores):
-    # Herbivores are located at North
-    x, y = territory.find_empty(0, 0, N//2, M)
-    animal = Herbivore(territory, x, y)
-    herbivores.append(animal)
+    for month in range(MAX_MONTH):
 
-carnivores = []
-for i in range(n_carnivores):
-    # Carnivores are located at South
-    x, y = territory.find_empty(N//2+1, 0, N, M)
-    animal = Carnivore(territory, x, y)
-    carnivores.append(animal)
+        # Information about this month
+        n_plants = sum([sum([int(territory.get(x, y).kind == PLANT) for y in range(M)]) for x in range(N)])
+        n_herbivores = len(herbivores)
+        n_carnivores = len(carnivores)
 
-print("Simulation with:")
-print(f"\t{N*M} territory area")
-print(f"\t{n_plants} plants")
-print(f"\t{n_herbivores} herbivores")
-print(f"\t{n_carnivores} carnivores")
-print(f"Timespan: {MAX_MONTH/12} years")
-print()
+        csvstring += f"{month}; {n_plants}; {n_herbivores}; {n_carnivores}\n"
+        print(f"{month}; {n_plants}; {n_herbivores}; {n_carnivores}")
+        if M*N < 4096:
+            print(territory)
 
-csvstring = "Time; Plants; Herbivores; Carnivores\n"
+        canvas.delete("all")
+        territory.display(canvas)
+        root.update()
 
+        # Stopping conditions
+        if n_plants == 0:
+            print("No more plants!")
+            break
+        if n_herbivores == 0:
+            print("No more herbivores!")
+            break
+        if n_carnivores == 0:
+            print("No more carnivores!")
+            break
 
-root = tk.Tk()
-root.geometry(f"{CELL_WIDTH*M}x{CELL_WIDTH*N}")
-root.title("Predator-prey simulation")
-canvas = tk.Canvas(root, width = CELL_WIDTH*M, height = CELL_WIDTH*N, bg = "white")
-canvas.pack(anchor = tk.CENTER)#, expand = True)
+        # Plants grow: randomly check for void cells and, if
+        # there are more than 2 plants around them, put a plant
+        # in them, too
+        for i in range(N*M):
+            x = randrange(0, N)
+            y = randrange(0, M)
+            if territory.get(x, y).kind == NIL:
+                # Counts the number of neighbourhoods: if 3 or
+                # more are plants, then here a plant shall born
+                if len(territory.find_near(x, y, PLANT)) > 2:
+                    territory.put(x, y, Plant())
 
-for month in range(MAX_MONTH):
+        # Animals do things
+        for species in [herbivores, carnivores]:
+            shuffle(species)
+            spawn = []
+            for animal in species:
+                animal.age()
+                if not (animal.is_dead() \
+                    or animal.can_mate(month, spawn) \
+                        or animal.can_eat()):
+                    animal.move_random()
+            species += spawn
 
-    # Information about this month    
-    n_plants = sum([sum([int(territory.get(x, y).kind == PLANT) for y in range(M)]) for x in range(N)])
-    n_herbivores = len(herbivores)
-    n_carnivores = len(carnivores)
-    
-    csvstring += f"{month}; {n_plants}; {n_herbivores}; {n_carnivores}\n"
-    print(f"{month}; {n_plants}; {n_herbivores}; {n_carnivores}")
-    if M*N < 4096:
-        print(territory)
+        # Dispose corpses: use explicitly indexed loop since the
+        # lists are changed inside the loop!
+        for species in [herbivores, carnivores]:
+            i = 0
+            while i < len(species):
+                if species[i].energy < 0:
+                    del species[i]
+                else:
+                    i += 1
 
-    canvas.delete("all")
-    territory.display(canvas)
-    root.update()
-    
-    # Stopping conditions
-    if n_plants == 0:
-        print("No more plants!")
-        break
-    if n_herbivores == 0:
-        print("No more herbivores!")
-        break
-    if n_carnivores == 0:
-        print("No more carnivores!")
-        break
-    
-    # Plants grow: randomly check for void cells and, if
-    # there are more than 2 plants around them, put a plant
-    # in them, too
-    for i in range(N*M):
-        x = randrange(0, N)
-        y = randrange(0, M)
-        if territory.get(x, y).kind == NIL:
-            # Counts the number of neighbourhoods: if 3 or
-            # more are plants, then here a plant shall born
-            if len(territory.find_near(x, y, PLANT)) > 2:
-                territory.put(x, y, Plant())
+    with open("lvts.csv", "w") as f:
+        f.write(str(csvstring))
+    print("Time series dumped on file lvts.csv")
 
-    # Animals do things
-    for species in [herbivores, carnivores]:
-        shuffle(species)
-        spawn = []
-        for animal in species:
-            animal.age()
-            if not (animal.is_dead() \
-                or animal.can_mate(month, spawn) \
-                    or animal.can_eat()):
-                animal.move_random()
-        species += spawn
-    
-    # Dispose corpses: use explicitly indexed loop since the
-    # lists are changed inside the loop!
-    for species in [herbivores, carnivores]:
-        i = 0
-        while i < len(species):
-            if species[i].energy < 0:
-                del species[i]
-            else:
-                i += 1
+simulation(N = 100,
+           M = 100,
+           MAX_MONTH = 512,
+           PLANT_RATIO = 0.5,
+           HERBIVORE_RATIO = 0.03,
+           CARNIVORE_RATIO = 0.01)
 
-with open("lvts.csv", "w") as f:
-    f.write(str(csvstring))
-print("Time series dumped on file lvts.csv")
